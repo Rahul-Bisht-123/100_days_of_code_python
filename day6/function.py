e581c153db09f1ac293b93832e6f60c73acebde3
Rahul-Bisht-123 <rbrahulbisht2000@gmail.com>
2024-05-24 13:44:38 +0530

    day6 completed

---
+++ day6/function.py
@@ -0,0 +1,9 @@
+def create_lassi(food_item):
+  print(f"welcome to {food_item} lassi recipe : \n")
+  print(f"1. Take 100g of {food_item} and 100ml curd")
+  print(f"2. Mix both the ingredients well")
+  print(f"3. Serve and eat You tasty {food_item} lassi\n")
+
+create_lassi("mango")
+create_lassi("apple")
+create_lassi("papaya")
